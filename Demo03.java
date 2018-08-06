47b05082fef8d27cc524cfd6ffcfb57984fa4ab7
zhang <2054029917@qq.com>
2018-08-06 17:21:15 +0800

    lianxi

---
+++ Demo03.java
@@ -0,0 +1,17 @@
+package text;
+
+public class Demo03 {
+	     public static void main(){
+	    	 String str="ABCD";
+	    	 //通过构造方法创建一个对象
+	    	 String str1=new String("ABCD");
+	    	 //2.
+	    	 String str2="ABC";
+	    	 //3.合并字符串
+	    	 String str3=str2+"D";
+	    	 System.out.println(str2);
+	     }
+	}
+
+
+
